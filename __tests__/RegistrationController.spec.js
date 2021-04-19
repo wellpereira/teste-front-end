bdf291aee76f2785924d5458652cd5e4b0ddbeb2
Wellington Pereira <wellington.pereira.wp@gmail.com>
2021-04-19 07:53:15 -0300

    Update RegistrationController.spec.js

--- __tests__/RegistrationController.spec.js
+++ __tests__/RegistrationController.spec.js
@@ -10,20 +10,3 @@ it("constructor RegistersList working", () => {
   new RegistrationController();
   expect(RegistersList).toHaveBeenCalledTimes(1);
 });
-
-// import Test from "../../helpers/Test";
-
-// it("save new register", () => {
-//   const registerFn = new RegistrationController();
-
-//   document.body.innerHTML = Test.form(
-//     registerFn,
-//     "Wellington",
-//     "111.222.333-99",
-//     "1122245554"
-//   );
-
-//   document.getElementById("save").submit();
-//   // const children = form.render().children().children();
-//   // form.simulate('submit', { target: { children } })
-// });
